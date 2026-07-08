c4427561dce95bd79e3e4944f7cd11638cae1c3f
Memory-Jiunian <xiangzhichen254@gmail.com>
2026-07-08 18:10:09 +0800

    chore(deploy): add github pages workflow

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: "/changshu-admin-terminal-demo/",
   plugins: [react()],
   resolve: {
     alias: {
@@ -11,4 +12,3 @@ export default defineConfig({
     },
   },
 });
-
